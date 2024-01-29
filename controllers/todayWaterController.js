7d6abfc30cd057c4bb4faf6c11e2c62e69a3c50b
Alexander Vorontsov <108517482+alexandrvorontsov@users.noreply.github.com>
2024-01-30 01:10:25 +0200

    del

--- controllers/todayWaterController.js
+++
@@ -1,5 +0,0 @@
-const { ctrlWrapper } = require("../helpers");
-
-const getTodayWater = async (req, res) => {};
-
-module.exports = { getTodayWater: ctrlWrapper(getTodayWater) };
--- controllers/waterController.js
+++
@@ -1,3 +0,0 @@
-const waterRateController = async (req, res) => {};
-
-module.exports = { waterRateController };
